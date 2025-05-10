cb241b0c044ad8b7e924cd86b2460f38ad69684d
JheisonGarcia1912 <castillojheison44@gmail.com>
2025-05-10 03:25:50 -0500

    Create Categories

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,18 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿// SupermarketWEB/Models/Category.cs
+using System.ComponentModel.DataAnnotations;
 
 namespace SupermarketWEB.Models
 {
     public class Category
     {
         [Key]
-        public int Id { get; set; } // Será la llave primaria
+        public int Id { get; set; }
 
         [Required]
+        [StringLength(80)]
         public string Name { get; set; }
 
+        [StringLength(200)]
         public string? Description { get; set; }
 
-        // Propiedad de navegación
-        public ICollection<Product>? Products { get; set; }
+        // Propiedad de navegación para los productos en esta categoría
+        public ICollection<Product>? Products { get; set; } = new List<Product>();
     }
 }
