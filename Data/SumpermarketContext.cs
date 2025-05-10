07690041d731ca22cce5c22a26d8f0e8829fbeed
JheisonGarcia1912 <castillojheison44@gmail.com>
2025-05-10 02:39:54 -0500

    Configurado DbContext y cadena de conexión

---
+++ Data/SumpermarketContext.cs
@@ -0,0 +1,16 @@
+﻿// SupermarketWEB/Data/SupermarketContext.cs
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SupermarketContext : DbContext
+    {
+        public SupermarketContext(DbContextOptions<SupermarketContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
