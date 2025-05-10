3dbd94f83d1ebd77a3c229558b7612de62a62f41
JheisonGarcia1912 <castillojheison44@gmail.com>
2025-05-10 02:15:26 -0500

    Agregados modelos Product y Category

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } // Será la llave primaria
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        // Propiedad de navegación
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; } // Será la llave foránea
+
+        // Propiedad de navegación
+        public Category? Category { get; set; }
+    }
+}
